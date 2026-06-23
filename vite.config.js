b1309222faf5e303a033adde4aede6de59144e6b
riotrip <riotriprayogo31@gmail.com>
2026-06-23 10:17:03 +0700

    fix: re-architecture vite build to dist

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  root: "docs",
-  base: "./",
-  esbuild: {
-    jsxInject: "import React from 'react'",
-  },
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-  },
-  server: {
-    open: true,
-  },
+  plugins: [react()],
+  base: "/",
 });
